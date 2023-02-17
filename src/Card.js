25709dcfac887583f5235f5b3ecf910c05e20b48
SametErdogan67 <samedi6774@gmail.com>
2023-02-18 00:15:30 +0300

    changes

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import "./Card.css";
 const Card = (data) => {
   return (
     <div className="card">
-      <img className="image" src={data.src.large} alt="image" />
+      <img src={data.src.large} alt="image" />
       <h3 className="photographer">{data.photographer}</h3>
     </div>
   );
